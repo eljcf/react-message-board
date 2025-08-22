3bd9f23b2fcf32aba00343f058d335b5d3d4a22f
eljcf <eljcfeljcf@outlook.com>
2025-08-22 16:15:42 +0800

    fix:vite base + router basename for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-message-board/',   // ← 这里必须是 “/你的仓库名/”
 })
